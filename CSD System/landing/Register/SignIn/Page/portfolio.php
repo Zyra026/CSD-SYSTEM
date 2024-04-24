adceb3c7e2e5ebc7b738c4237b365287f1550dc6
Zyra026 <142954905+Zyra026@users.noreply.github.com>
2024-04-24 18:04:42 +0800

    Add files via upload

---
+++ CSD System/landing/Register/SignIn/Page/portfolio.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="css/portfolio.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+</head>
+<body>
+   <div class="container">
+       <div class="profile-card"> 
+            <div class="profile-pic">
+                <img src="imahe/profile/prof1.png" alt="user avatar">
+            </div>
+
+            <div class="profile-details">
+                 <div class="intro">
+                    <h2>Jerry B. Agsunod</h2>
+                    <h4>BSCS</h4>
+                    
+                 </div>
+
+                 <div class="contact-info">
+                    <div class="row">
+                         <div class="icon">
+                            <i class="fa fa-phone"  style="color:var(--light-green)"></i>
+                         </div>
+                         <div class="content">
+                            <span>Phone</span>
+                            <h5>+639157012384 </h5>
+                         </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="icon">
+                           <i class="fa fa-envelope-open"  style="color:var(--light-green)"></i>
+                        </div>
+                        <div class="content">
+                           <span>Email</span>
+                           <h5>  jethroyd@gmail.com</h5>
+                        </div>
+                   </div>
+    
+                   <div class="row">
+                    <div class="icon">
+                       <i class="fa fa-map-marker"  style="color:var(--light-purple)"></i>
+                    </div>
+                    <div class="content">
+                       <span>Location</span>
+                       <h5>Linao, Libon, Albay</h5>
+                    </div>
+                 </div>
+
+            </div>
+         </div>
+       </div>
+
+       <div class="about">
+           <h1>Schedule</h1>
+
+          <div class="work">
+             <div class="workbox">
+                 <div class="icon">
+                    <img src="images/ui.svg" alt="">
+                 </div>
+                 <div class="desc">
+                    <h3>What I Play</h3>
+                    <p><li>Valorant</li>
+                     <li>League of Legends</li>
+                     <li>Call of Duty Mobile</li>
+                     <li>Mobile Legends</li>
+                     <li>Farlight</li>
+                    </p>
+                 </div>
+             </div>
+
+             <div class="workbox">
+                <div class="icon">
+                   <img src="images/app.svg" alt="">
+                </div>
+                <div class="desc">
+                   <h3>Favorite Artists(Singer)</h3>
+                   <p><li>Adie</li>
+                     <li>Arthur Nery</li>
+                     <li>Ben&Ben</li>
+                     <li>Toneejay</li>
+                     <li>Juan Karlos</li>
+                     <li>Beabadoobee</li>
+                  </p>
+                </div>
+            </div>
+
+            <div class="workbox">
+                <div class="icon">
+                   <img src="images/api.svg" alt="">
+                </div>
+                <div class="desc">
+                   <h3>Skills</h3>
+                   <p><li>I....I play guitar:D</li>
+                     <li>Magreklamo</li>
+                  </p>
+                </div>
+            </div>
+
+            <div class="workbox">
+                <div class="icon">
+                   <img src="images/web.svg" alt="">
+                </div>
+                <div class="desc">
+                   <h3>Socials</h3>
+                   <p>
+                     
+                     Facebook: Jeth Roy L. Delos Santos<br>
+                     Instagram: troy.eet<br>
+                     Discord: troyeet
+                   </p>
+                </div>
+            </div>
+          </div>
+       </div>
+   </div>
+</body>
+</html>
